9e35276466a56a27daab5b831e9503b56679780a
pablo3345 <pabloandresperuchi@gmail.com>
2024-06-15 02:07:00 -0300

    empezando el curso de django Rest 3

--- django_rest2/ecommerce_rest/users/api/api.py
+++ django_rest2/ecommerce_rest/users/api/api.py
@@ -17,6 +17,23 @@ from rest_framework.decorators import api_view #un decorador para las vistas bas
         
     
 #una vista basada en funcion
+'''@api_view(['GET', 'POST']) # va GET o POST (puedo usar uno o ambos) xq es el metodo http que va a permitir
+def user_api_view(request): # es una vista basada en funcion x eso va request
+    
+   if request.method == 'GET':
+        
+        users = User.objects.all()
+        userSerializer = UserSerializer(users, many=True)# con meny=True, le digo que no me traiga un users, sino todo el listado de users, aca me lo convierte en json
+        
+        return Response(userSerializer.data) # pongo userSerializer xq esa variable me contiene el json y agregarle .data para que me retorne el json
+    
+    
+   elif request.method=='POST': # sino lo envia por get puede enviarlo por post y desde la pagina puedo crear un superusuario pasandole el json 
+       print(request.data) # en request.data almacena los datos del POST '''
+       
+       
+
+#una vista basada en funcion
 @api_view(['GET', 'POST']) # va GET o POST (puedo usar uno o ambos) xq es el metodo http que va a permitir
 def user_api_view(request): # es una vista basada en funcion x eso va request
     
@@ -25,13 +42,39 @@ def user_api_view(request): # es una vista basada en funcion x eso va request
         users = User.objects.all()
         userSerializer = UserSerializer(users, many=True)# con meny=True, le digo que no me traiga un users, sino todo el listado de users, aca me lo convierte en json
         
-        return Response(userSerializer.data) # pongo userSerializer xq esa variable me contiene el json y agregarle .data para que me retorne el json'''
+        return Response(userSerializer.data) # pongo userSerializer xq esa variable me contiene el json y agregarle .data para que me retorne el json
     
     
-   elif request.method=='POST': # sino lo envia por get puede enviarlo por post y desde la pagina puedo crear un superusuario pasandole el json
-       print(request.data) # en request.data almacena los datos del POST
-       
-       
-    
+   elif request.method=='POST': # sino lo envia por get puede enviarlo por post y desde la pagina puedo crear un superusuario pasandole el json 
+      user_serializer = UserSerializer(data = request.data) # creo la variable y con el serializador creo un modelo en json
+      if user_serializer.is_valid(): # aca antes en django comun funcionado un Form a igual para los errores
+          user_serializer.save()
+          return Response(user_serializer.data) # cuando el serializador guarda la informacion se guarda en data y con return retorna dicha informacion
+      
+      return Response(user_serializer.errors) # si no es valido, va a retornar los errores y ubicar esos errores
+          
+        
         
 
+@api_view(['GET', 'PUT', 'DELETE']) # para obtener un usuario con el id, para actualizar en Rest se usa el metodo PUT
+def user_detail_view(request, pk=None):
+    if request.method=='GET':
+       user = User.objects.filter(id =pk).first() # first seria el primer elemento
+       # aca podria hacerlo diferente ej: poner un mensaje si el pk en None, o podria hacerlo como en django comun
+       user_serializer = UserSerializer(user) # aca le paso el usuario y no pongo many=True xq quiero obtener un solo objeto y no la lista completa de usuarios
+       return Response(user_serializer.data)
+   
+    elif request.method=='PUT': # para actualizar un usuario en rest se usa el metodo PUT
+         user = User.objects.filter(id =pk).first() # first seria el primer elemento
+         user_serializer=UserSerializer(user, data=request.data) # sea PUT o POST con request.data llega la informacion al serielizador
+         # a diferencia del POST aca en el PUT le envio el user que es la instacia que se va actualizar y luego el data=request.data
+         if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        
+         return Response(user_serializer.errors) # aca a esto lo pone como el elif del if de is_valid, pero sin el elif
+     
+    elif request.method == 'DELETE':
+          user = User.objects.filter(id =pk).first() # first seria el primer elemento
+          user.delete()
+          return Response('Eliminado!')
--- django_rest2/ecommerce_rest/users/api/urls.py
+++ django_rest2/ecommerce_rest/users/api/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import path
 #from users.api.api import UserApiView
-from users.api.api import user_api_view
+from users.api.api import user_api_view, user_detail_view
 
 urlpatterns = [
     #path('usuario/', UserApiView.as_view(), name ='usuarioApi') url basada en clase
-    path('usuario/', user_api_view, name ='usuarioApi') # url basada en funcion
+    path('usuario/', user_api_view, name ='usuarioApi'), # url basada en funcion
+    path('usuario/<int:pk>/', user_detail_view, name ='usuario_api_detail_api_view') # url basada en funcion
 ]
